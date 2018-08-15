7a45aa88e9a6e5f892842fa60801d7561b00b820
lishican <lishican2014@outlook.com>
2018-08-15 19:38:44 +0800

    in

--- test.js
+++
@@ -1,8 +0,0 @@
-let socket = new socketHandle({
-  url: "ws:/127.0.0.1:8081/websocket/" + uid,
-  maxReconnect: 3,
-  reconnectInterval: 2000,
-  timeoutInterval: 2000,
-  maxReconnectInterval: 4000,
-  heartInfo: "ping"
-});
